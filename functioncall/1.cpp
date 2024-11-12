cc4c5cff7737ad560fbe6296e954752f4d85c760
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-12 21:42:03 +0500

    update

--- functioncall/1.cpp
+++ functioncall/1.cpp
@@ -1,1 +1,21 @@
-#include
+#include <iostream>
+using namespace std;
+
+void sum(int a, int b) {
+    int result = a + b;
+    cout << result<<'\t';
+}
+
+int add() {
+    int a = 12;
+    int b = 23;
+    sum(a, b);
+    return a;
+}
+
+int main() {
+	int w;
+    w = add();
+    cout<<w;
+    return 0;
+}
