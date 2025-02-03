edb7422049ba0ea6fea0699f5ee1939c72420af5
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-03 11:57:20 +0500

    update

--- Backtracking/aubset.cpp
+++ Backtracking/aubset.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+void subset(vector<int> &arr, vector<int> &ans;int i){
+    if (i== arr.size()){
+        for (int val: ans){
+            cout <<val;
+        }
+        cout<<endl;
+    }
+}
 
-int main()
+    int main()
 {
+    vector<int> arr = {1,2,3,4,5};
+    vector<int> ans ;
+
+    return 0;
 }
