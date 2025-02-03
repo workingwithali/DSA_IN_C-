c2ed7d39932074137fe04a4497a249693616257c
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-03 11:59:54 +0500

    update

--- Backtracking/aubset.cpp
+++ Backtracking/aubset.cpp
@@ -7,13 +7,19 @@ void subset(vector<int> &arr, vector<int> &ans;int i){
             cout <<val;
         }
         cout<<endl;
+        return;
     }
+    ans.push_back(arr[i]);
+    subset(arr,ans,i+1);
+    ans.pop_back();
+    subset(arr,ans,i+1);
 }
 
     int main()
 {
     vector<int> arr = {1,2,3,4,5};
     vector<int> ans ;
+    subset(arr,ans,0);
 
     return 0;
 }
