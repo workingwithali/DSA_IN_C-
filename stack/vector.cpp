e0a5353a9b0ea02f29dae1467c00dc6cb39abfdb
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-03-07 16:45:56 +0500

    update

--- stack/vector.cpp
+++ stack/vector.cpp
@@ -13,13 +13,22 @@ class stack{
 		int top(){
 			return v[v.size()-1];
 		}
+		bool empty(){
+			return v.size() == 0;
+		}
 	
 };
 int main(){
 	stack s;
 	s.push(10);
-	s.push(30);
 	s.push(20);
+	s.push(30);
+	while(!s.empty()){
+		cout <<s.top();
+		s.pop();
+	}
+	cout<<' '<<endl;
+	
 	
 	return 0;
 }
