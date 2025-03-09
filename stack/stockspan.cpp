c62b7422828fa6171a42dc0d3a1970645ea8dab5
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-03-09 14:47:40 +0500

    update

---
+++ stack/stockspan.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+int main(){
+	vector<int> price ={200,100,80,60,70,80,30,20,100};
+	int n = price.size();
+	
+	vector<int> ans(n,0);
+	stack<int> s;
+	for(int i=0;i<n;i++){
+		wh
+	}
+	
+	return 0;
+}
