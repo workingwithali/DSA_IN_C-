e07213254b3b0c2d4600fcec2720ddedae443696
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-03-11 10:16:34 +0500

    update

--- stack/greatelement.cpp
+++ stack/greatelement.cpp
@@ -1,15 +1,24 @@
-#include<isotream>
+#include<iostream>
 #include<vector>
 #include<stack>
 
 using namespace std;
 int main(){
 	vector<int> arr={6,8,0,1,3};
-	vector<int> ans;
+	vector<int> ans(arr.size(),-1);
 	stack<int> s;
-	for(int i=arr.size();i>=0;i--){
-		while()
+	for(int i=arr.size()-1;i>=0;i--){
+		while(!s.empty()&&s.top()<=arr[i]){
+			s.pop();
+		}
+		if(!s.empty()){	
+			ans[i]=s.top();
+		}
+		s.push(arr[i]);
 	}
-	
+	for(int val:ans){
+		
+		cout <<val<<' ';
+	}
 	return 0;
 }
