ae77cb781f84ba1581cc772bb329c27510f8ee3a
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-06-04 11:22:11 +0500

    update

--- binarytree/preorder.cpp
+++ binarytree/preorder.cpp
@@ -1,32 +1,51 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-class Node {
-	public:
-		int data;
-		Node* left;
-		Node* right;
-		
-		Node(int val){
-			data = val;
-			left = right = nullptr;
-		}
-		
-};
-static int idx =-1
-Node *buildTree(int preorder){
-	idx++;
-	if(preorder[idx]==-1){
-		return nullptr;
-	}
-	
-}
-int main(){
-	int s = 4;
-	int* ptr = &s;
 
-	cout << *(ptr)<<endl;
-	
-	return 0;
-	
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+// Global index for preorder traversal
+int idx = -1;
+
+// Use reference to vector<int>
+Node* buildTree(const vector<int>& preorder) {
+    idx++;
+    if (idx >= preorder.size() || preorder[idx] == -1) {
+        return nullptr;
+    }
+
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+    return root;
+}
+
+// Helper function to print inorder traversal
+void inorder(Node* root) {
+    if (root == nullptr) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main(){
+    vector<int> preorder = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    
+    Node* root = buildTree(preorder);
+
+    cout << "Inorder Traversal: ";
+    inorder(root);
+    cout << endl;
+
+    return 0;
 }
