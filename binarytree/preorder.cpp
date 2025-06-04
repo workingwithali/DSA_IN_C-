391817cb15b2a9567f0767290ba8cf0d45f0d81e
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-06-04 10:23:43 +0500

    update

---
+++ binarytree/preorder.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node {
+	public:
+		int data;
+		Node* left;
+		Node* right;
+		
+		Node(int val){
+			data = val;
+			left = right = nullptr;
+		}
+		
+};
+int main(){
+	
+	return 0;
+	
+}
